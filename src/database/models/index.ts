cc6bc5fcb10618abb3a164d767a70250b0e05ed6
dviluk <dviluk@outlook.jp>
2022-04-24 22:47:31 -0500

    chore(database): watermelondb deleted

--- src/database/models/index.ts
+++
@@ -1,9 +0,0 @@
-export {default as Difficulty} from './Difficulty';
-export {default as Equipment} from './Difficulty';
-export {default as Exercise} from './Exercise';
-export {default as Muscle} from './Muscle';
-export {default as PivotExerciseEquipment} from './PivotExerciseEquipment';
-export {default as PivotExerciseMuscle} from './PivotExerciseMuscle';
-export {default as Plan} from './Plan';
-export {default as Workout} from './Workout';
-export {default as WorkoutLog} from './WorkoutLog';
