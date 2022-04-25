5d09e2556843b4aa094c2b68e7c1829d6a98e245
dviluk <dviluk@outlook.jp>
2022-04-24 22:47:55 -0500

    wip: app entities (models)

--- src/@app.d.ts
+++ src/@app.d.ts
@@ -1,5 +1,5 @@
 declare namespace App {
-  namespace Models {
+  namespace Entities {
     interface Difficulty {
       id: string;
       name: string;
@@ -14,6 +14,8 @@ declare namespace App {
       id: string;
       name: string;
       description: string;
+      difficulty_id: string;
+      tag_id: string; // to group variants
       // muscles: Muscle[];
       // equipment: Equipment[];
     }
@@ -28,28 +30,62 @@ declare namespace App {
       name: string;
       difficulty_id: string;
       description: string;
-      // workouts: Workout[];
+      weeks: number;
+      // routines: Workout[];
+    }
+
+    interface Routine {
+      id: string;
+      name: string;
+      description: string;
+      plan_id: string;
+      day: number;
+      order: number;
+      // workouts: WorkoutRoutine
+    }
+
+    interface RoutineWorkout {
+      routine_id: string;
+      workout_id: string;
     }
 
     interface Workout {
       id: string;
       exercise_id: string;
+      difficulty_id: string;
       plan_id: string;
       sets: number;
       rest: number;
       min_repetitions: number;
       max_repetitions: number;
       // logs?: WorkoutLog[];
-      // userLogs?: WorkoutLog[];
     }
 
     interface WorkoutLog {
       id: string;
+      user_id: string;
       workout_id: string;
+      plan_id: string;
+      user_plan_id: string;
       set_number: number;
       repetitions: number;
       rested: number;
       time: number;
     }
+
+    interface Users {
+      //
+    }
+    interface UserPlans {
+      id: string;
+      plan_id: string;
+      user_id: string;
+      progress: number;
+      start_date: string;
+      end_date: string;
+    }
+
+    // Para guardar información de gráficas y no tener que recarlular los valores
+    // interface Statistics {}
   }
 }
